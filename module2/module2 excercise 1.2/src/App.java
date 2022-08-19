45ba5f327b20d89e0073ade472bd7dae8b6e37aa
Shadarien Williams <shadarienwilliams@gmail.com>
2022-08-19 17:29:13 -0400

    first upload

---
+++ module2/module2 excercise 1.2/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args){
+        int numOfMessages = 5;
+        String message = "Welcome to Java";
+        for (int i=0; i < numOfMessages; i++){
+            System.out.println(message);
+        }
+    }
+}
+
