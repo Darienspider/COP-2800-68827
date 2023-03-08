3874448efb131430bcd66916474fda3ed3242bf7
Shadarien Williams <shadarien@shadwilliams.dev>
2023-03-07 21:05:02 -0500

    12.5 (Write/read data) Write a program to create a file named Exercise12_15.txt if it does not exist.

--- module12/textIOAssignment/src/App.java
+++ module12/textIOAssignment/src/App.java
@@ -1,5 +1,35 @@
+import java.io.File;  
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.*;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int minimum = 0;
+        int maximum = 100;
+
+        File outputFile = new File("/home/shadarien/java_course/COP-2800-68827/module12/textIOAssignment/src/output.txt");
+        FileWriter writer = new FileWriter(outputFile);
+        FileReader reader = new FileReader(outputFile);
+        Scanner datafeed = new Scanner(reader);
+
+        for (int i = 0; i < 100; i++) {
+            int randomNumber = (int) (Math.random() * (maximum - minimum + 1) + minimum); 
+            writer.write(randomNumber + " ");
+        }
+        
+        // Close the FileWriter object after writing all the data
+        writer.close();
+
+        // Read the data back from the file and display it in increasing order
+        ArrayList characterStorage = new ArrayList<>();
+        while (datafeed.hasNextInt()){
+            int foundNumber = datafeed.nextInt();
+            characterStorage.add(foundNumber);
+        }
+        reader.close();
+        Collections.sort(characterStorage);
+        System.out.println(characterStorage);
     }
 }
